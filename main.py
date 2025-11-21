7d4cba9758e669a3d2759014c82cb7fc0d14c4c9
AlmostSurely <atomfried11@mailbox.org>
2025-11-21 22:40:47 +0100

    added roration to the player model

--- main.py
+++ main.py
@@ -19,12 +19,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000  
+        
         screen.fill("black")
+        player.update(dt)
+
         player.draw(screen)
         pygame.display.flip()
-        # limit framerate to 60 FPS
-        dt = clock.tick(60) / 1000
+
     
 
 if __name__ == "__main__":
